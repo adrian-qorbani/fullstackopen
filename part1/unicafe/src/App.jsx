1f6393b56957b9a4e997bcede0425c0efe49b4bf
Adrian <mahdigh@live.com>
2023-10-06 14:06:17 +0330

    exercise 1.7 done. Basic statistics added.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,40 +1,73 @@
 import { useState } from "react";
-import "./App.css";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState([]);
 
   const onClickGood = () => {
-    console.log(good)
+    console.log(good);
     setGood(good + 1);
+    setTotal(total + 1);
+    setAverage(average.concat(1));
+    console.log(average);
   };
 
   const onClickNeutral = () => {
-    console.log(neutral)
+    console.log(neutral);
     setNeutral(neutral + 1);
+    setTotal(total + 1);
+    setAverage(average.concat(0));
+    console.log(average);
   };
 
   const onClickBad = () => {
-    console.log(bad)
+    console.log(bad);
     setBad(bad + 1);
+    setTotal(total + 1);
+    setAverage(average.concat(-1));
+    console.log(average);
+  };
+
+  const calcAverage = (array) => {
+    if (array.length === 0) {
+      return "Not Enough Votes.";
+    }
+    return array.reduce((a, b) => a + b, 0) / array.length;
+  };
+
+  const calcPercentage = (array) => {
+    if (array.length === 0) {
+      return 0;
+    }
+    const totalNoOfVotes = array.length;
+    const positives = array.filter((vote) => vote > 0);
+    return (positives.length * 100) / totalNoOfVotes;
   };
 
   return (
     <>
       <h1>Unicafe</h1>
       <h3>Give Feedback</h3>
-      <div className="card">
-        <button onClick={() => onClickGood()}>Good</button>
-        <button onClick={() => onClickNeutral()}>Neutral</button>
-        <button onClick={() => onClickBad()}>Bad</button>
+      <div>
+        <Button handleClick={onClickGood} text="Good" />
+        <Button handleClick={onClickNeutral} text="Neutral" />
+        <Button handleClick={onClickBad} text="Bad" />
       </div>
       <h3>Statistics</h3>
       <div>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Average: {calcAverage(average)}</p>
+        <p>Percentage: {calcPercentage(average)} %</p>
       </div>
     </>
   );
