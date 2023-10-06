f1fae4ac11e15a2fd25d374e1e4892b8fce7dc80
Adrian <mahdigh@live.com>
2023-10-06 12:59:21 +0330

    exercise 1.6 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,22 +2,39 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickGood = () => {
+    console.log(good)
+    setGood(good + 1);
+  };
+
+  const onClickNeutral = () => {
+    console.log(neutral)
+    setNeutral(neutral + 1);
+  };
+
+  const onClickBad = () => {
+    console.log(bad)
+    setBad(bad + 1);
+  };
 
   return (
     <>
       <h1>Unicafe</h1>
       <h3>Give Feedback</h3>
       <div className="card">
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button onClick={() => onClickGood()}>Good</button>
+        <button onClick={() => onClickNeutral()}>Neutral</button>
+        <button onClick={() => onClickBad()}>Bad</button>
       </div>
       <h3>Statistics</h3>
       <div>
-        <p>Good:</p>
-        <p>Neutral:</p>
-        <p>Bad:</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
       </div>
     </>
   );
