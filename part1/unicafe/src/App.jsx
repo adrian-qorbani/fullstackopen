d048cec23f6c17811cbf0fbed2980d607311f9c4
Adrian <mahdigh@live.com>
2023-10-07 07:42:38 +0330

    exercises 1.8 - 1.9 done. added statistics as a seprate component with conditional rendering

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,29 +1,53 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
+const Statistics = ({ votes }) => {
+  const total = votes.good + votes.neutral + votes.bad;
+  const average = (votes.good + votes.bad * -1) / total;
+  const positive = votes.good * (100 / total);
+
+  if (total === 0) {
+    return (
+      <>
+      <p>No Feedback Given.</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>Good: {votes.good}</p>
+      <p>Neutral: {votes.neutral}</p>
+      <p>Bad: {votes.bad}</p>
+      <p>Average: {average}</p>
+      <p>Percentage: {positive}%</p>
+    </>
+  );
+};
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
 function App() {
-
   const [votes, setVote] = useState({
-    good: 0, neutral: 0, bad:0
+    good: 0,
+    neutral: 0,
+    bad: 0,
   });
 
   const handleGoodVote = () => {
-    setVote({...votes, good: votes.good + 1});
+    setVote({ ...votes, good: votes.good + 1 });
     console.log(votes);
   };
 
   const handleNeutralVote = () => {
-    setVote({...votes, neutral: votes.neutral + 1})
+    setVote({ ...votes, neutral: votes.neutral + 1 });
     console.log(votes);
   };
 
   const handleBadVote = () => {
-    setVote({...votes, bad: votes.bad + 1})
+    setVote({ ...votes, bad: votes.bad + 1 });
     console.log(votes);
   };
 
@@ -37,14 +61,7 @@ function App() {
         <Button handleClick={handleBadVote} text="Bad" />
       </div>
       <h3>Statistics</h3>
-      <div>
-        {/* <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p> */}
-        {/* <p>Total: {total}</p>
-        <p>Average: {calcAverage(average)}</p>
-        <p>Percentage: {calcPercentage(average)} %</p> */}
-      </div>
+      <Statistics votes={votes} />
     </>
   );
 }
