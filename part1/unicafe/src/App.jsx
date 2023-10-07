1b66508b926895fe0ed66340866cb5c5fcf6004e
Adrian <mahdigh@live.com>
2023-10-07 08:31:47 +0330

    More components. Improved aesthetics.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 
+// Variables
+const PAGE_TITLE = "Unicafe";
+
+// Page Title Component
+const Title = ({ text }) => <h1>{text}</h1>;
+
+// Page Headers Component
+const Header = ({ text }) => <h3>{text}</h3>;
+// Page Desc
+const PageDesc = () => (
+  <h4>
+    A Cafeteria Feedback System made in React by Adrian |{" "}
+    <a href="https://github.com/adrian-qorbani/fullstackopen/tree/main/part1/unicafe">
+      Github Repo
+    </a>
+  </h4>
+);
+
+// Single Statistic Component
 const StatisticLine = ({ data, value }) => {
-  return (
+  return data === "Positives:" ? (
+    <tr>
+      <td>
+        {data} {value} %
+      </td>
+    </tr>
+  ) : (
     <tr>
       <td>
         {data} {value}
@@ -11,11 +36,13 @@ const StatisticLine = ({ data, value }) => {
   );
 };
 
+// Statistics Component
 const Statistics = ({ votes }) => {
   const total = votes.good + votes.neutral + votes.bad;
   const average = (votes.good + votes.bad * -1) / total;
   const positive = votes.good * (100 / total);
 
+  // if there aren't any votes ...
   if (total === 0) {
     return (
       <>
@@ -40,17 +67,21 @@ const Statistics = ({ votes }) => {
   );
 };
 
+// Button Component
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+// App Component
 function App() {
+  // All three kind of votes used within a useState
   const [votes, setVote] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
+  // Event Handles defined
   const handleGoodVote = () => {
     setVote({ ...votes, good: votes.good + 1 });
     console.log(votes);
@@ -66,16 +97,16 @@ function App() {
     console.log(votes);
   };
 
+  // App Render
   return (
     <>
-      <h1>Unicafe</h1>
-      <h3>Give Feedback</h3>
-      <div>
-        <Button handleClick={handleGoodVote} text="Good" />
-        <Button handleClick={handleNeutralVote} text="Neutral" />
-        <Button handleClick={handleBadVote} text="Bad" />
-      </div>
-      <h3>Statistics</h3>
+      <Title text={PAGE_TITLE} />
+      <PageDesc />
+      <Header text="Give Feedback" />
+      <Button handleClick={handleGoodVote} text="Good" />
+      <Button handleClick={handleNeutralVote} text="Neutral" />
+      <Button handleClick={handleBadVote} text="Bad" />
+      <Header text="Statistics" />
       <Statistics votes={votes} />
     </>
   );
