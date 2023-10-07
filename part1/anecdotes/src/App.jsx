84dc4d439738a551b084c99b16c7d8a39da8bb85
Adrian <mahdigh@live.com>
2023-10-07 09:33:59 +0330

    exercise 1.13 done.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,22 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// Anecdote Renderer Component
+const AnecdoteDisplay = ({index}) => {
+  console.log(index)
+  return (
+    <>
+      <p>{index}</p>
+    </>
+  )
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
 
   return (
     <>
       <h1>Software Engineering Anecdotes</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+          Next Anecdote
         </button>
+        <AnecdoteDisplay index={anecdotes[selected]}/>
       </div>
-      <p className="read-the-docs">
-        Learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
