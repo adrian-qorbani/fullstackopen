ef612061f03829f3688751253dea932125655d95
Adrian <mahdigh@live.com>
2023-10-08 13:58:02 +0330

    exercise 2.9 finished. You can serach for names and they show up.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,51 +2,85 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("Marty Byrd");
+  const [newNumber, setNewNumber] = useState("000-000-000");
+  const [showPerson, setShowPerson] = useState("");
 
   // adding new name to form
   const addNewName = (e) => {
     e.preventDefault();
-    console.log(newName);
+    console.log(newName, newNumber);
 
     // check for duplicate name
-    if (persons.find(x => x.name == newName)) {
+    if (persons.find((x) => x.name == newName)) {
       alert(`${newName} is already added to the phonebook.`);
       return;
     }
-    
+
     // define a new contact
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
     };
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
   };
 
-  // on change handler
-  const handleInputChange = (e) => {
+  // on change handlers
+  const handleNameInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+  const handleNumberInputChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+  const handleSearchInputChange = (e) => {
+    setShowPerson(e.target.value);
     console.log(e.target.value);
-    setNewName(e.target.value)
-  }
+  };
+
+  // filter search
+  // const personsToShow = showPerson ? persons : persons.filter(persons => persons.name === showPerson)
+  // const personsToShow = persons.filter(persons => persons.name === showPerson)
 
   // renderer
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Search: <input value={showPerson} onChange={handleSearchInputChange} />
+      </div>
+      <h2>Add new contact:</h2>
       <form onSubmit={addNewName}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          Name: <input value={newName} onChange={handleNameInputChange} />
         </div>
         <div>
-          <button type="submit">
-            add
-          </button>
+          Number: <input value={newNumber} onChange={handleNumberInputChange} />
+        </div>
+        <div>
+          <button type="submit">ADD</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =><p key={person.name}>{person.name}</p>)}
+        {persons
+          .filter((person) => {
+            return showPerson.toLowerCase() === ""
+              ? person
+              : person.name.toLowerCase().includes(showPerson);
+          })
+          .map((person) => (
+            <p key={person.name}>
+              {person.name} * {person.number}
+            </p>
+          ))}
       </div>
     </div>
   );
