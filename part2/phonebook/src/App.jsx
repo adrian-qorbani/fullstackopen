4a0259e09a93b14c72c99b962a7fe7f8377f6fa0
Adrian <mahdigh@live.com>
2023-10-08 11:21:45 +0330

    exercise 2.6 done.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import Note from './components/Note'
+import { useState } from "react";
+import "./App.css";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("Marty Byrd");
+
+  // adding new name to form
+  const addNewName = (e) => {
+    e.preventDefault();
+    console.log("on change event listner works");
+
+    const personObject = {
+      name: newName
+    };
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  };
+
+  // on change handler
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value)
+  }
+
+  // renderer
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person =><p key={person.name}>{person.name}</p>)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
