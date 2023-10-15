1041c0f42ea62770d99a9bc364806d59109da09b
Adrian <mahdigh@live.com>
2023-10-15 11:47:39 +0330

    added fontawesome. visuals enhanced.

--- part2/countries_info/src/components/Footer.jsx
+++ part2/countries_info/src/components/Footer.jsx
@@ -1,24 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGear,
+  faCode,
+  faPaintbrush,
+} from "@fortawesome/free-solid-svg-icons";
+
+const element = <FontAwesomeIcon icon={faPaintbrush} />;
+const element1 = <FontAwesomeIcon icon={faCode} />;
+const element2 = <FontAwesomeIcon icon={faGear} />;
+
 const Footer = () => {
   return (
     <footer id="footer">
-    <p>
-      Created and designed by{" "}
-      <a href="">
-        Adrian Ghorbani <i className="fa fa-link"></i>
-      </a>{" "}
-      2023 |{" "}
-      <a href="">
-        {" "}
-        Source Code <i className="fa fa-github"></i>
-      </a>{" "}
-      |{" "}
-      <a href="https://picocss.com/">
-        {" "}
-        Theme <i className="fa fa-github"></i>
-      </a>
-    </p>
-  </footer>
-  )
-}
+      <p>
+        Created and designed by <a href="">{element2} Adrian Ghorbani</a> 2023 |{" "}
+        <a href=""> {element1} Source Code</a> |{" "}
+        <a href="https://picocss.com/"> {element} Theme </a>
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- part2/countries_info/src/components/Header.jsx
+++ part2/countries_info/src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+
+const element0 = <FontAwesomeIcon icon={faGlobe} />;
 const Header = () => {
   return (
     <hgroup>
-      <h1>Rest Countries</h1>
+      <h1>{element0} Rest Countries</h1>
       <h3>Countries Information Website</h3>
     </hgroup>
   );
