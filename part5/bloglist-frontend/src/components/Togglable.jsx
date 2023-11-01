53a7919ddadc1667b3fcda42f6e3215eb5c995b9
Adrian <mahdigh@live.com>
2023-11-01 10:00:48 +0330

    added proptypes to login and toggleable.

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,20 +1,21 @@
-import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from "prop-types";
+import { useState, forwardRef, useImperativeHandle } from "react";
 
 const Togglable = forwardRef((props, refs) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
 
   useImperativeHandle(refs, () => {
     return {
-      toggleVisibility
-    }
-  })
+      toggleVisibility,
+    };
+  });
 
   return (
     <div>
@@ -26,7 +27,11 @@ const Togglable = forwardRef((props, refs) => {
         <button onClick={toggleVisibility}>Cancel</button>
       </div>
     </div>
-  )
-})
+  );
+});
 
-export default Togglable
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+export default Togglable;
