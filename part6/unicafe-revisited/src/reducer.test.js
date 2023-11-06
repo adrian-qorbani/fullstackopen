89043547b01f83b318aab0b0a17be28b0bd0da91
Adrian <mahdigh@live.com>
2023-11-06 11:47:37 +0330

    fixed zero bug which turned the state into NaN.

--- part6/unicafe-revisited/src/reducer.test.js
+++ part6/unicafe-revisited/src/reducer.test.js
@@ -62,4 +62,19 @@ describe("unicafe reducer", () => {
       bad: 1,
     });
   });
+
+  test("zero resets the stats", () => {
+    const action = {
+      type: "ZERO",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
+    });
+  });
 });
