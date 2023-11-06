7e983f50fb76f8a1b60b09824a56dd70111a4fe6
Adrian <mahdigh@live.com>
2023-11-06 08:40:22 +0330

    exercises 6.1-6.2 done. added redux to unicafe + testing.

--- part6/unicafe-revisited/src/main.jsx
+++ part6/unicafe-revisited/src/main.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => store.dispatch({ type: "GOOD" })}>good</button>
+      <button onClick={() => store.dispatch({ type: "OK" })}>ok</button>
+      <button onClick={() => store.dispatch({ type: "BAD" })}>bad</button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>
+        reset stats
+      </button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
