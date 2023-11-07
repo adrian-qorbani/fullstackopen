1fc38e8ad3224662eadf9bc255d3b1287aac0c2e
Adrian <mahdigh@live.com>
2023-11-07 10:28:13 +0330

    exercise 6.5 done. anecdotes are sorted by number of votes.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -7,28 +7,30 @@ const App = () => {
 
   // Voting action creator
   const addVote = (id) => {
-    dispatch(vote(id))
-  }
+    dispatch(vote(id));
+  };
 
   const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(addAnecdotes(content))
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    dispatch(addAnecdotes(content));
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => addVote(anecdote.id)}>vote</button>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => addVote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div>
