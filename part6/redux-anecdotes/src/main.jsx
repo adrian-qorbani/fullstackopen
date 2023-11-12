f389fc0b883dc011df53862dbd078ee896e683eb
Adrian <mahdigh@live.com>
2023-11-12 09:39:24 +0330

    exercise 6.10 done. moved redux store creation witihin store.js in src dir.

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,28 +1,7 @@
 import ReactDOM from 'react-dom/client'
-// import { createStore, combineReducers } from 'redux'
-
 import { Provider } from 'react-redux'
-
-import { configureStore } from '@reduxjs/toolkit'
-
 import App from './App'
-
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-// const reducer = combineReducers({
-//   anecdotes: anecdoteReducer,
-//   filter: filterReducer
-// })
-
-// const store = createStore(reducer)
-
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    filter: filterReducer
-  }
-})
+import store from './store'
 
 console.log(store.getState())
 
