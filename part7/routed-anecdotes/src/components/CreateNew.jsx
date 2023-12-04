cf8357e6f6150f40d83ba9e740846d61f6babace
Adrian <mahdigh@live.com>
2023-12-04 21:32:32 +0330

    added ultimate-hooks base files.

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import { useField } from "../hooks";
 
 const CreateNew = (props) => {
-  // const [content, setContent] = useState("");
-  // const [author, setAuthor] = useState("");
-  // const [info, setInfo] = useState("");
   const content = useField("text");
   const author = useField("text");
   const info = useField("text");
