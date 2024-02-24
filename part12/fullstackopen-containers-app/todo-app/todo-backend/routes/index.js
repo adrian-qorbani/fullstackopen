cb5c8c9096f97ceb3179cf730931ee848e9fe789
Mahdi <mahdigh@live.com>
2024-02-24 21:43:14 +0330

    exercise 12-10: fixed connection refused error on localhost.

--- part12/fullstackopen-containers-app/todo-app/todo-backend/routes/index.js
+++ part12/fullstackopen-containers-app/todo-app/todo-backend/routes/index.js
@@ -16,11 +16,20 @@ router.get('/', async (req, res) => {
 });
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const value = await redis.getAsync("added_todos");
-  res.json({
-      added_todos: Number(value) || 0
-  });
-});
+// router.get('/', async (_, res) => {
+//   const value = await redis.getAsync("added_todos");
+//   res.json({
+//       added_todos: Number(value) || 0
+//   });
+// });
+
+router.get("/statistics", async (_, res) => {
+  console.log('1', await redis.getAsync("todoCount"))
+  const count = Number((await redis.getAsync("todoCount")) || "0")
+
+  res.send({
+    added_todos: count,
+  })
+})
 
 module.exports = router;
--- part12/fullstackopen-containers-app/todo-app/todo-backend/routes/todos.js
+++ part12/fullstackopen-containers-app/todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const redis = require("../redis")
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,8 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+  const count = Number((await redis.getAsync("todoCount")) || "0")
+  await redis.setAsync("todoCount", count + 1)
   res.send(todo);
 });
 
