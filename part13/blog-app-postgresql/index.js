3aeb3d41cd5086885604f741404879d82d6cd61c
Mahdi <mahdigh@live.com>
2024-02-28 12:48:01 +0330

    exercise 13-1 done. blog postgresql server built with express.

---
+++ part13/blog-app-postgresql/index.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    // ssl: {
+    //   require: true,
+    //   rejectUnauthorized: false
+    // }
+  },
+});
+
+app.use(express.json());
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+// GET ALL Blogs
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+// CREATE A Blog
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
