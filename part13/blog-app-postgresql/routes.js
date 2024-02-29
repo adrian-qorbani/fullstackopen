d005239bff06bda05721a687e2db592af22c471a
Mahdi <mahdigh@live.com>
2024-02-29 08:24:39 +0330

    exercise 13.4: added DELETE route as well.

--- part13/blog-app-postgresql/routes.js
+++ part13/blog-app-postgresql/routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/api/blogs", async (req, res) => {
   try {
     const blogs = await Blog.findAll();
+    console.log(JSON.stringify(notes, null, 2));
     res.json(blogs);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -20,6 +21,32 @@ router.post("/api/blogs", async (req, res) => {
     return res.json(blog);
   } catch (error) {
     return res.status(400).json({ error: error.message });
+  }
+});
+
+// GET single blog
+router.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log("BLOG:", blog.toJSON());
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// DELETE a blog
+router.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    if (blog) {
+      await blog.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
