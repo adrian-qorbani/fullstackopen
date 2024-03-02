83eebe1e7f6bb7c987efe63f4e9a52d3a3edc70e
Mahdi <mahdigh@live.com>
2024-03-02 09:20:42 +0330

    exercise 13.22: nt functionality in the application to mark a blog in the reading list as read added.

--- part13/blog-app-postgresql/controllers/readingList.js
+++ part13/blog-app-postgresql/controllers/readingList.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { Blog, User } = require("../models");
+const { Blog, User, ReadingList } = require("../models");
 const router = express.Router();
 
 router.post('/api/readinglists', async (req, res) => {
     try {
       const { blogId, userId } = req.body;
   
-      // Check if the user and blog exist
       const user = await User.findByPk(userId);
       const blog = await Blog.findByPk(blogId);
   
@@ -14,13 +13,33 @@ router.post('/api/readinglists', async (req, res) => {
         return res.status(404).json({ message: 'User or blog not found' });
       }
   
-      // Add blog to the user's reading list
       await user.addBlog(blog);
   
       return res.status(200).json({ message: 'Blog added to reading list' });
     } catch (error) {
       console.error('Error adding blog to reading list:', error);
       return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
+  router.put("/:id", async (req, res) => {
+    try {
+      const readingListId = req.params.id;
+      const { read } = req.body;
+  
+      const readingListEntry = await ReadingList.findByPk(readingListId);
+  
+      if (!readingListEntry) {
+        return res.status(404).json({ message: 'Reading list entry not found' });
+      }
+  
+      readingListEntry.unread = !read; 
+      await readingListEntry.save();
+  
+      res.status(200).json({ message: 'Reading list entry updated successfully' });
+    } catch (error) {
+      console.error('Error updating reading list entry:', error);
+      res.status(500).json({ message: 'Internal server error' });
     }
   });
 
