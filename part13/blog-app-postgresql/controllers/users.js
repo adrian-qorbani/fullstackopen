a3ac082ff8b1995a469549eb39e0e7f1a7072917
Mahdi <mahdigh@live.com>
2024-03-02 08:36:13 +0330

    added ability to allow admin to change status of users.

--- part13/blog-app-postgresql/controllers/users.js
+++ part13/blog-app-postgresql/controllers/users.js
@@ -30,22 +30,63 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+router.post("/", async (request, response) => {
+  const body = request.body;
 
-  const user = await User.findOne({ where: { username } });
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+  const user = await User.findOne({
+    where: {
+      username: body.username,
+    },
+  });
+
+  const passwordCorrect = body.password === "secret";
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: "invalid username or password",
+    });
   }
 
-  const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
-  if (!passwordCorrect) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+  if (user.disabled) {
+    return response.status(401).json({
+      error: "account disabled, please contact admin",
+    });
   }
 
-  const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
 
-  res.json({ token });
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  if (!user.admin) {
+    return res.status(401).json({ error: "operation not allowed" });
+  }
+  next();
+};
+
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
